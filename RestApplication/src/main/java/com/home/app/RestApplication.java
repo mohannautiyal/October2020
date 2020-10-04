53e1716b88904e8bee57a68b5de119247cf787ba
mohannautiyal <mohannautiyal80@gmail.com>
2020-10-04 14:46:43 +0530

    RestApplication

---
+++ RestApplication/src/main/java/com/home/app/RestApplication.java
@@ -0,0 +1,14 @@
+package com.home.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+	
+public static void main(String[] args)  {
+	SpringApplication.run(RestApplication.class, args);
+}
+
+}
