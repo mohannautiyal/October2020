dd03b8e586f5799d1cbe9dedfe8d27bdd683abbe
mohannautiyal <mohannautiyal80@gmail.com>
2020-10-04 22:53:10 +0530

    App completed

--- RestApplication/src/main/java/com/home/app/RestApplication.java
+++ RestApplication/src/main/java/com/home/app/RestApplication.java
@@ -2,9 +2,10 @@ package com.home.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
 
 	
 public static void main(String[] args)  {
