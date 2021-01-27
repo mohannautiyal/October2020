cb7327fb2ab819efa15156a8adee9c40517fa854
mohannautiyal <mohannautiyal80@gmail.com>
2021-01-27 21:28:50 +0530

    Update WelcomeController.java

--- RestApplication/src/main/java/com/home/app/controllers/WelcomeController.java
+++ RestApplication/src/main/java/com/home/app/controllers/WelcomeController.java
@@ -11,7 +11,7 @@ public class WelcomeController {
 	@GetMapping(value="/")
 	public String welcomeEmp() {
 		
-		return "Welcome to the Home Page by Maddy";
+		return "Welcome to the Home Page by Maddy. Doing another change";
 	}
 
 }
