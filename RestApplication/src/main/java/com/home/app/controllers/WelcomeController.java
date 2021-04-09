ecde960c673d065d12d5b147111ce6f9a2a53a1b
mohannautiyal <mohannautiyal80@gmail.com>
2021-04-09 13:39:10 +0530

    Update WelcomeController.java

--- RestApplication/src/main/java/com/home/app/controllers/WelcomeController.java
+++ RestApplication/src/main/java/com/home/app/controllers/WelcomeController.java
@@ -11,7 +11,7 @@ public class WelcomeController {
 	@GetMapping(value="/")
 	public String welcomeEmp() {
 		
-		return "Welcome to the Home Page by Maddy. This is a CI and CD project Pipeline Deployment";
+		return "Welcome to Madan's Home Page. This is a CI and CD project Pipeline Deployment";
 	}
 
 }
